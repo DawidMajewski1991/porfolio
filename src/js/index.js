eb4437f3d2b9ae13ee3718a0628b5930c151691a
Dawid Majewski <dawidmajewski1991@gmail.com>
2022-09-20 10:58:59 +0200

    desktop version

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,37 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
+console.log("Hi! I,m Dawid. Nice to meet you!");
+fetch(
+  "https://api.github.com/users/dawidmajewski1991/repos?sort=created&direction=asc"
+)
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res);
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+    const container = document.querySelector(".projects__grid--js");
+    for (let repo of res) {
+      const { description, homepage, html_url, name } = repo;
 
-/* place your code below */
-
-console.log('HELLO 🚀')
+      const template = `<article class="repository">
+    <div class="repository__top"></div>
+    <img class="repository__icon" alt="GitHub Logo."></img>
+    <div class="repository__grid">
+    <div class="repository__labels">
+      <p class="repository__text repository__text--right">project:</p>
+      <p class="repository__text repository__text--right">description:</p>
+      <p class="repository__text repository__text--right">demo:</p>
+      <p class="repository__text repository__text--right">github:</p>
+    </div>
+    <div class="repository__titles">
+      <p class="repository__text repository__text--left repository__text--white">${name}</p>
+      <p class="repository__text repository__text--left">${description}</p>
+      <p><a class="repository__text repository__text--left repository__text--blue" href="${homepage}">see_here</a></p>
+      <p><a class="repository__text repository__text--left repository__text--blue" href=${html_url}>see_here</a></p>
+    </div>
+  </div>
+</article>`;
+      if (description) {
+        container.innerHTML += template;
+      }
+    }
+  })
+  .catch((e) => console.log(e));
